dbd12af5e0491e9e7e8e4081237cbf5fbbe9f517
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-16 12:46:25 +0100

    Updated code

--- src/LCC_Node_Component_Base.h
+++ src/LCC_Node_Component_Base.h
@@ -9,7 +9,15 @@
  */
 
  class LCC_Node_Component_Base {
+  public:
 
+
+  protected:
+    void setSendEventCallbackFunction(void (*sendEvent)(uint16_t eventIndexToSend)) { this->sendEvent = sendEvent; }
+
+  private:
+    // Call back function to send events.
+    void (*sendEvent) (uint16_t eventIndexToSend);
 
  };
 
