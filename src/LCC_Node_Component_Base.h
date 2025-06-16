e82ee6914e6597b49f01f3af3aa6a22053c92177
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-16 14:45:03 +0100

    Added pure virtual functions

--- src/LCC_Node_Component_Base.h
+++ src/LCC_Node_Component_Base.h
@@ -12,6 +12,13 @@
   public:
     void setSendEventCallbackFunction(void (*sendEvent)(uint16_t eventIndexToSend)) { this->sendEvent = sendEvent; }
 
+    /**
+     * Pure virtual functions to force them to be overridden in derived classes.
+     */
+    virtual bool eventIndexMatches(uint16_t index) = 0;
+    virtual bool eventIndexMatchesCurrentState(uint16_t index) = 0;
+    virtual void sendEventsForCurrentState() = 0;
+
   protected:
     // Call back function to send events.
     void (*sendEvent) (uint16_t eventIndexToSend);
