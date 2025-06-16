c4b4689a0477f062fa5ff6165f6f4694dc9a5bb5
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-16 10:49:19 +0100

    Initial commit

---
+++ src/LCC_Node_Component_Base.h
@@ -0,0 +1,16 @@
+#ifndef LCC_NODE_COMPONENT_BASE_H
+#define LCC_NODE_COMPONENT_BASE_H
+
+/**
+ * The base class for the following LCC node components;-
+ * - LCC_Servo
+ * - Frog
+ * - TOTI
+ */
+
+ class LCC_Node_Component_Base {
+
+
+ };
+
+ #endif
